0871562ea3b32ec3567d09a4411528e2db5b9532
shrikantgcet <shri.kant@fmg.co.nz>
2022-06-17 10:52:59 +1200

    test

---
+++ variables.tf
@@ -0,0 +1,25 @@
+
+
+
+
+/*
+  **********************Creds used for azurerd**********************
+*/
+variable "aad_tenant_id" {
+  type        = string
+  description = "Azure AD provider tenant ID"
+}
+
+variable "aad_client_id" {
+  type        = string
+  description = "Azure AD provider client ID"
+}
+
+variable "aad_client_secret_id" {
+  type        = string
+  description = "Azure AD provider secret ID"
+}
+
+/*
+  **********************Ends*******************************
+*/
