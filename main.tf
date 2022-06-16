a207676ba51d0b61d0c022c42fdaac6d3f36e685
shrikantgcet <shri.kant@fmg.co.nz>
2022-06-17 10:30:27 +1200

    test

--- main.tf
+++ main.tf
@@ -1,12 +1,29 @@
+# terraform {
+#   required_providers {
+#     azapi = {
+#       source  = "azure/azapi"
+#       version = "= 0.2.0"
+#     }
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "=3.5.0"
+#     }
+#   }
+# }
+
+
 terraform {
   required_providers {
-    azapi = {
-      source  = "azure/azapi"
-      version = "= 0.2.0"
-    }
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.5.0"
+      version = "~> 3.2.0"
+    }
+  }
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "TerraformDemolocal"
+
+    workspaces {
+      name = "terraform-app-reg"
     }
   }
 }
@@ -18,5 +35,14 @@ provider "azurerm" {
   features {}
 }
 
+
+provider "azuread" {
+  alias = "azuread_fmg_aad_group_admin"
+
+  tenant_id     = var.aad_tenant_id
+  client_id     = var.aad_client_id
+  client_secret = var.aad_client_secret_id
+
+}
 
 data "azuread_client_config" "current" {}
